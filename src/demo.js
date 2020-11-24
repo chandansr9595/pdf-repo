a8afb120d8c9d19cd9b2e8a4635a5a77371e34e6
chandansr9595 <chandan95sr@gmail.com>
2020-11-24 11:16:36 +0530

    init commit

---
+++ src/demo.js
@@ -0,0 +1,21 @@
+import React, {useEffect} from "react";
+import Button from "@material-ui/core/Button";
+import { openPDF } from "./openPDF";
+import { base64string } from "./pdf.js";
+
+export default function Demo() {
+
+  useEffect(() => {
+    onOpenPDF();
+  });
+
+  const password = '123456';
+  const onOpenPDF = () => {
+    openPDF(base64string, password);
+  };
+  return (
+    <Button variant="contained" color="primary" onClick={() => onOpenPDF()}>
+      Open PDF
+    </Button>
+  );
+}
